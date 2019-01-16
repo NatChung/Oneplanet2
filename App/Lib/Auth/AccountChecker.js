4afa0265209dd8bb1313b79c58cf7c00555541d1
Nat <nat.chung@mores.com.tw>
2019-01-16 16:24:53 +0800

    [F] Fixed twitter without email taht is registered with other email

--- App/Lib/Auth/AccountChecker.js
+++ App/Lib/Auth/AccountChecker.js
@@ -1,5 +1,5 @@
 
-import { getUser } from '../../../src/graphql/queries'
+import { getUser, getTwitterExtEmail, getFbExtEmail } from '../../../src/graphql/queries'
 import { GoogleSignin } from 'react-native-google-signin'
 import { RNTwitterSignIn } from 'react-native-twitter-signin'
 import { 
@@ -10,48 +10,49 @@ import {
 
 class AccountChecker{
 
-    twitter = client => new Promise(resolve => {
-        let userData = null
-        RNTwitterSignIn.logOut()
-
-        RNTwitterSignIn.logIn()
-            .then(loginData => {
-                console.tron.log(loginData)
-                const {email, userName, userID} = loginData
-                if(!email) return resolve({
-                    params: {
-                        nickname: userName,
-                        id:userID,
-                        subType:'twitter',
-                        type:'addEmail'
-                    }
-                })
-
-                userData = loginData
-                return client.query({
-                    query: getUser,
-                    variables: {id: email}, 
-                    fetchPolicy:'network-only'
-                })
+    twitterWithEmail = ({email, userName}, resolve, client) => {
+        client.query({
+            query: getUser,
+            variables: {id: email}, 
+            fetchPolicy:'network-only'
+        })
+        .then(({data}) => {
+            if(data.getUser) resolve({error: {message:'theEamilOfTwitterAlredyInUsed'}})
+            else resolve({
+                params:{
+                    email,
+                    nickname:userName,
+                    type:'twitter'
+                },
             })
-            .then(({data}) => {
-                const {email, userName} = userData
-                resolve({
-                    error:(data && data.getUser) ? {message:'theEmailAlredyInUsed'} : null,
-                    params:{
-                        email,
-                        nickname:userName,
-                        type:'twitter'
-                    },
-                })
-            })
-            .catch(err => {
-                resolve({
-                    error:err.toString()
-                })
-            })
+        })
+        .catch(err => {
+            resolve({error:{message: err.toString()}})
+        })
+    }
 
-    })
+    twitterWithoutEmail = ({userID, userName}, resolve, client) => {
+        client.query({
+            query: getTwitterExtEmail,
+            variables: {id: userID}, 
+            fetchPolicy:'network-only'
+        })
+        .then(({data}) => {
+            console.tron.log('withoutEmail', data)
+            if(data.getTwitterExtEmail) resolve({ error:{message:'theTwitterAlredyRegistered'}})
+            else resolve({
+                params:{
+                    nickname: userName,
+                    id:userID,
+                    subType:'twitter',
+                    type:'addEmail'
+                }
+            })
+        })
+        .catch(err => {
+            resolve({error:{message: err.toString()}})
+        })
+    }
 
     addEmail = (client, {email}, {avatarPath, nickname, subType, id}) => new Promise(resolve => {
         client.query({
@@ -81,44 +82,73 @@ class AccountChecker{
         })
     })
 
-    _fbGetInfoCallBack = (error, result, resolve, client) => {
-        LoginManager.logOut()
-        if(error) return resolve({error: error.toString()})
-
-        const {email, name, picture, id} = result
-        if(!email) return resolve({
-            params: {
-                nickname: name,
-                avatarPath: picture.data.url,
-                id,
-                subType:'fb'
-            }
-        })
-
+    fbWithEmail = ({email, name, picture}, resolve, client) => {
         client.query({
             query: getUser,
             variables: {id: email}, 
             fetchPolicy:'network-only'
         })
         .then(({data}) => {
-            // console.tron.log(data)
-            resolve({
-                error:(data && data.getUser) ? {message:'theEmailAlredyInUsed'} : null,
+            if (data.getUser) resolve({error:{message:'theFbAlredyRegistered'} })
+            else resolve({
                 params:{
                     email,
-                    nickname: name,
                     avatarPath: picture.data.url,
-                    id,
+                    nickname:name,
                     type:'fb'
                 },
             })
         })
         .catch(err => {
-            // console.tron.log(err)
-            resolve({
-                error:{message: err.toString()}
+            resolve({error:{message: err.toString()}})
+        })
+    }
+
+    fbWithoutEmail = ({ name, picture, id}, resolve, client) => {
+        client.query({
+            query: getFbExtEmail,
+            variables: {id}, 
+            fetchPolicy:'network-only'
+        })
+        .then(({data}) => {
+            if(data.getFbExtEmail) resolve({ error:{message:'theFbAlredyRegistered'}}) 
+            else resolve({
+                params:{
+                    nickname: name,
+                    avatarPath: picture.data.url,
+                    id,
+                    subType:'fb',
+                    type:'addEmail'
+                }
             })
         })
+        .catch(err => {
+            // console.tron.log(err)
+            resolve({error:{message: err.toString()}})
+        })
+    }
+
+    
+
+    twitter = client => new Promise(resolve => {
+        RNTwitterSignIn.logOut()
+        RNTwitterSignIn.logIn()
+            .then(loginData => {
+                console.tron.log('loginData', loginData)
+                if(loginData.email) this.twitterWithEmail(loginData, resolve, client)
+                else this.twitterWithoutEmail(loginData, resolve, client)
+            })
+            .catch(err => {
+                console.tron.log('twitterError', err)
+                resolve({error:err.toString()})
+            })
+    })
+
+    fbGetInfoCallBack = (error, result, resolve, client) => {
+        LoginManager.logOut()
+        if(error) return resolve({error: error.toString()})
+        if(result.email) this.fbWithEmail(result, resolve, client)
+        else this.fbWithoutEmail(result, resolve, client)
     }
 
     fb = client => new Promise(resolve => {
@@ -127,15 +157,13 @@ class AccountChecker{
             .then(result => {
                 if(result.isCancelled) {
                     LoginManager.logOut()
-                    return resolve({
-                        error:'canceled',
-                    })
+                    return resolve({error:'canceled'})
                 }
                 
                 const infoRequest = new GraphRequest(
                     '/me?fields=name,picture,email',
                     null,
-                    (err, ret) => this._fbGetInfoCallBack(err, ret, resolve, client),
+                    (err, ret) => this.fbGetInfoCallBack(err, ret, resolve, client),
                 )
 
                 new GraphRequestManager().addRequest(infoRequest).start()
