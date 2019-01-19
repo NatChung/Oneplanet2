de2b1411afcb00edb3034ef3937a56cf5a93de5e
Max <max.liu0325@gmail.com>
2019-01-19 15:14:05 +0800

    [M] Modify Terms screem to modal

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,11 +1,11 @@
 import { createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import ForgetPasswordScreen from '../Containers/ForgetPasswordScreen'
-import AddEmailScreen from '../Containers/AddEmailScreen'
-import TermsScreen from '../Containers/TermsScreen'
-import EmailSentScreen from '../Containers/EmailSentScreen'
-import AddProfileScreen from '../Containers/AddProfileScreen'
-import SignupScreen from '../Containers/SignupScreen'
+import ForgetPasswordScreen from '../Containers/ForgetPasswordScreen';
+import AddEmailScreen from '../Containers/AddEmailScreen';
+import TermsScreen from '../Containers/TermsScreen';
+import EmailSentScreen from '../Containers/EmailSentScreen';
+import AddProfileScreen from '../Containers/AddProfileScreen';
+import SignupScreen from '../Containers/SignupScreen';
 import TestAppSyncContainer from '../Containers/TestAppSyncContainer';
 import TestCognitoContainer from '../Containers/TestCognitoContainer';
 import LaunchScreen from '../Containers/LaunchScreen';
@@ -14,35 +14,56 @@ import LandingScreen from '../Containers/LandingScreen';
 import LoginScreen from '../Containers/LoginScreen';
 
 // Manifest of possible screens
-const PrimaryNav = createStackNavigator({
-  	ForgetPasswordScreen: { screen: ForgetPasswordScreen },
-  	AddEmailScreen: { screen: AddEmailScreen },
-	TermsScreen: { screen: TermsScreen },
-	SignupScreen: { screen: SignupScreen },
-	LoginScreen: { screen: LoginScreen },
-	LandingScreen: { screen: LandingScreen },
-},{
-	// Default config for all screens
-	initialRouteName: 'LandingScreen',
-});
+const PrimaryNav = createStackNavigator(
+	{
+		ForgetPasswordScreen: { screen: ForgetPasswordScreen },
+		AddEmailScreen: { screen: AddEmailScreen },
+		SignupScreen: { screen: SignupScreen },
+		LoginScreen: { screen: LoginScreen },
+		LandingScreen: { screen: LandingScreen }
+	},
+	{
+		// Default config for all screens
+		initialRouteName: 'LandingScreen'
+	}
+);
 
-const ContentNav = createStackNavigator({
-	LaunchScreen: { screen: LaunchScreen }
-},{
-	initialRouteName: 'LaunchScreen',
-})
+const ContentNav = createStackNavigator(
+	{
+		LaunchScreen: { screen: LaunchScreen }
+	},
+	{
+		initialRouteName: 'LaunchScreen'
+	}
+);
 
+const PrimarySwitch = createSwitchNavigator(
+	{
+		EmailSentScreen: { screen: EmailSentScreen },
+		AddProfileScreen: { screen: AddProfileScreen },
+		TutorialScreen: { screen: TutorialScreen },
+		TestAppSyncContainer: { screen: TestAppSyncContainer },
+		TestCognitoContainer: { screen: TestCognitoContainer },
+		PrimaryNav,
+		ContentNav
+	},
+	{
+		initialRouteName: 'PrimaryNav'
+	}
+);
 
-const PrimarySwitch = createSwitchNavigator({
-	EmailSentScreen: { screen: EmailSentScreen },
-	AddProfileScreen: { screen: AddProfileScreen },
-	TutorialScreen: { screen: TutorialScreen },
-	TestAppSyncContainer: { screen: TestAppSyncContainer },
-	TestCognitoContainer: { screen: TestCognitoContainer },
-	PrimaryNav,
-	ContentNav
-},{
-	initialRouteName: 'PrimaryNav'
-})
+const RootStack = createStackNavigator(
+	{
+		PrimarySwitch: { screen: PrimarySwitch },
+		TermsScreen: { screen: TermsScreen }
+	},
+	{
+		initialRouteName: 'PrimarySwitch',
+		mode: 'modal',
+		headerMode: 'none'
+	}
+);
 
-export default createAppContainer(PrimarySwitch);
+// ignite g component TabNavigationView
+
+export default createAppContainer(RootStack);
